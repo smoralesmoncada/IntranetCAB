d245e6f3d4e4bb74a783bb54bae183d51edb25ee
smoralesmoncada <smorale@alumnos.ubiobio.cl>
2019-04-17 00:48:25 -0400

    conexion

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'sqlsrv:Server=DESKTOP-LB8A26U\SQLEXPRESS;Database=prueba',
+    'username' => 'sa',
+    'password' => 'rr_hh',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
